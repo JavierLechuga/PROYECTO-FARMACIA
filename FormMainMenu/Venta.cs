2183b6d365e977b511d61fc215014afb54b41b0b
Javier Ignacio <javier.quinteros@inacapmail.cl>
2016-11-05 21:54:20 -0300

    Venta | Número de venta modificado (XML)

--- FormMainMenu/Venta.cs
+++ FormMainMenu/Venta.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml;
 
 namespace FormMainMenu
 {
     public partial class Venta : Form
     {
+        public XDocument xmldoc = XDocument.Load(Application.StartupPath + "\\archivos\\numeroventa.xml");
+        public string numeroventas = Application.StartupPath + "\\archivos\\numeroventa.xml";
+
         public Venta()
         {
             InitializeComponent();
@@ -22,7 +28,14 @@ namespace FormMainMenu
             lblFecha.Text = DateTime.Now.ToString("dd/MM/yyyy HH:mm");
             lblNombreUsuario.Text = Login.currentUser;
             lblRutUsuario.Text = Login.rutUser;
-            int venta = 0;
+            int numeroventaint = 0;
+
+            XElement numeroventa = xmldoc.Descendants("NumeroVenta").FirstOrDefault(p => p.Element("numero").Value == numeroventaint.ToString());
+            if (numeroventa != null)
+            {
+                lblNumeroVenta.Text = numeroventa.Element("numero").Value;
+            }
+
 
         }
     }
